97c8c3174ddccd491d8169bff25a219bf4dcdea7
Daniel Furman <daniel.furman@codilime.com>
2017-09-25 14:16:25 +0200

    Symlink commands for ET

--- symlink.sh
+++ symlink.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 # Symlink configuration files on Linux system
 
-COMMON_FILES=".bashrc .gitconfig .gitignore_global .profile"
-EXTRA_FILES=".gitconfig_local .profile_local"
-
 DOTFILES_PATH=$PWD
 FILES_PATH=$DOTFILES_PATH/files
+GAMES_PATH=$FILES_PATH/games
+
+if [ -v DEBUG ]; then
+	set -x
+fi
 
 if [ -v COMMON ]; then
 	echo "Creating symbolic links to common configs"
+	COMMON_FILES=".bashrc .gitconfig .gitignore_global .profile"
 	for f in $COMMON_FILES; do
 		ln -s $OPTS $FILES_PATH/$f $HOME/$f
 	done
@@ -16,6 +19,7 @@ fi
 
 if [ -v EXTRA ]; then
 	echo "Creating symbolic link to extra configs"
+	EXTRA_FILES=".gitconfig_local .profile_local"
 	for f in $EXTRA_FILES; do
 		ln -s $OPTS $FILES_PATH/$f $HOME/$f
 	done
@@ -39,4 +43,30 @@ if [ -v VSCODE ]; then
 
 	ln -s $OPTS $VSCODE_SRC/settings.json $HOME/.config/Code/User/settings.json
 	ln -s $OPTS $VSCODE_SRC/extensions $HOME/.vscode/
+fi
+
+if [ -v ET ]; then
+	echo "Creating symbolic links to ET client configs"
+	ET_SRC=$GAMES_PATH/etlegacy
+	ET_DEST=$HOME/.etlegacy
+
+	mkdir -p $ET_DEST/legacy/profiles/Fenthick/
+	ln -s $OPTS $ET_SRC/autoexec.cfg $ET_DEST/legacy/profiles/Fenthick/
+	ln -s $OPTS $ET_SRC/legacy/etconfig.cfg $ET_DEST/legacy/profiles/Fenthick/
+fi
+
+ET_SERVER_FILES="campaigncycle.cfg  etl_server.cfg  legacy.cfg  lmscycle.cfg  mapvotecycle.cfg  objectivecycle.cfg  punkbuster.cfg  server.cfg  stopwatchcycle.cfg"
+
+if [ -v ET_SERVER32 ]; then
+	echo "Creating symbolic links to ET server 32-bit configs"
+	for f in $ET_SERVER_FILES; do
+		ln -s $OPTS $GAMES_PATH/etlegacy/etmain/$f $HOME/opt/etlegacy-v2.75-i386/etmain/
+	done
+fi
+
+if [ -v ET_SERVER64 ]; then
+	echo "Creating symbolic links to ET server 64-bit configs"
+	for f in $ET_SERVER_FILES; do
+		ln -s $OPTS $GAMES_PATH/etlegacy/etmain/$f $HOME/opt/etlegacy-v2.75-x86_64/etmain/
+	done
 fi
