42a4bca507a122599ec2cfe3c1e1b8946ac897fa
Daniel Furman <fenthick92@gmail.com>
2017-12-03 22:50:44 +0100

    Create symlink target for CS 1.6

--- symlink.sh
+++ symlink.sh
@@ -14,7 +14,7 @@ if [ -v COMMON ]; then
 	echo "Creating symbolic links to common configs"
 	COMMON_FILES=".bashrc .gitconfig .gitignore_global .profile"
 	for f in $COMMON_FILES; do
-		ln -s $OPTS $FILES_PATH/$f $HOME/$f
+		ln -s $OPTS $FILES_PATH/$f ~/$f
 	done
 fi
 
@@ -22,16 +22,16 @@ if [ -v EXTRA ]; then
 	echo "Creating symbolic link to extra configs"
 	EXTRA_FILES=".gitconfig_local .profile_local"
 	for f in $EXTRA_FILES; do
-		ln -s $OPTS $FILES_PATH/$f $HOME/$f
+		ln -s $OPTS $FILES_PATH/$f ~/$f
 	done
 
-	ln -s $OPTS $FILES_PATH/config $HOME/.ssh/config
+	ln -s $OPTS $FILES_PATH/config ~/.ssh/config
 fi
 
 if [ -v SUBLIME ]; then
 	echo "Creating symbolic links to Sublime Text 3 configs"
 	SUBLIME_SRC=$FILES_PATH/sublime-text-3
-	SUBLIME_DEST=$HOME/.config/sublime-text-3
+	SUBLIME_DEST=~/.config/sublime-text-3
 
 	ln -s $OPTS $SUBLIME_SRC/Installed\ Packages $SUBLIME_DEST/
 	ln -s $OPTS $SUBLIME_SRC/Packages $SUBLIME_DEST/
@@ -41,22 +41,29 @@ if [ -v VSCODE ]; then
 	echo "Creating symbolic links to Visual Studio Code configs"
 	VSCODE_SRC=$FILES_PATH/vscode
 
-	ln -s $OPTS $VSCODE_SRC/settings.json $HOME/.config/Code/User/settings.json
-	ln -s $OPTS $VSCODE_SRC/extensions $HOME/.vscode/
+	ln -s $OPTS $VSCODE_SRC/settings.json ~/.config/Code/User/settings.json
+	ln -s $OPTS $VSCODE_SRC/extensions ~/.vscode/
+fi
+
+if [ -v CS ]; then
+	echo "Creating symbolic links to CS 1.6 configs"
+	CS_SRC=$GAMES_PATH/cs16
+	CS_DST=~/.steam/steam/steamapps/common/Half-Life/cstrike
+
+	ln -s $CS_SRC/config.cfg $CS_DST/
+	ln -s $CS_SRC/userconfig.cfg $CS_DST/
 fi
 
 if [ -v ET ]; then
-	echo "Creating symbolic links to ET client configs"
-	ET_SRC=$GAMES_PATH/et
-	ET_DEST=$HOME/.etlegacy
+	echo "Creating symbolic links to ET configs"
 
-	ln -s $OPTS $ET_SRC/autoexec.cfg $ET_DEST/etmain/
+	ln -s $OPTS $GAMES_PATH/et/autoexec.cfg ~/.etlegacy/etmain/
 fi
 
 if [ -v ET_SERVER ]; then
 	echo "Creating symbolic links to ET server configs"
 	ET_SERVER_SRC=$GAMES_PATH/et/server
-	ET_DEST=$HOME/.etlegacy
+	ET_DEST=~/.etlegacy
 
 	ln -s $OPTS $ET_SERVER_SRC/etl_server.cfg $ET_DEST/etmain/
 	ln -s $OPTS $ET_SERVER_SRC/legacy.cfg $ET_DEST/etmain/
