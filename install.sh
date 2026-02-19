65f1c28ecdba2e7a7d735ab8c8b8a09c77636fa8
Daniel Furman <danielfurman8@gmail.com>
2026-02-19 12:09:56 +0100

    Extract setup_mac

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ usage() {
     echo -e "Install and configure development tools on Mac/Linux/WSL.\n"
     echo "Options:"
     echo -e "\t--dotfiles       => Configure dotfiles"
-    echo -e "\t--mac            => Setup Mac"
     echo -e "\t--ohmyzsh        => Install Oh My ZSH"
     echo -e "\t--vscode         => Install VS Code extensions"
     echo -e "\t--vscode-save    => Save VS Code extensions to file"
@@ -28,7 +27,6 @@ fi
 while :; do
     case "$1" in
         --dotfiles) dotfiles=1; shift;;
-        --mac) mac=1; shift;;
         --ohmyzsh) ohmyzsh=1; shift;;
         --vscode) vscode=1; shift;;
         --vscode-save) vscode_save=1; shift;;
@@ -47,7 +45,6 @@ run() {
     [ -n "$force_symlink" ] && LN="ln -sfv"
 
     [ -n "$dotfiles" ] && setup_dotfiles
-    [ -n "$mac" ] && setup_mac
     [ -n "$ohmyzsh" ] && install_ohmyzsh
     [ -n "$vscode" ] && install_vscode_extensions
     [ -n "$vscode_save" ] && save_vscode_extensions
@@ -98,178 +95,6 @@ setup_dotfiles() {
         ${LN} "$files_path/vscode/keybindings.json" "${HOME}/.config/Code - OSS/User/keybindings.json"
         ${LN} "$files_path/vscode/keybindings.json" "${HOME}/.config/Cursor/User/keybindings.json"
     fi
-}
-
-setup_mac() {
-    ## Appearance
-
-    # Appearance -> allow wallpaper tinting in windows: disable
-    defaults write NSGlobalDomain AppleReduceDesktopTinting -bool true
-
-    # Click in the scrollbar to jump to the spot that's clicked
-    defaults write NSGlobalDomain AppleScrollerPagingBehavior -int 1
-
-    ## Dock, Menu bar and Mission Control
-
-    # Desktop & Dock > enable autohide; setup delay and animation speed
-    defaults write com.apple.dock autohide -bool true
-    defaults write com.apple.dock autohide-delay -float 0
-    defaults write com.apple.dock autohide-time-modifier -float 0.5
-
-    # Control Centre -> Menu Bar Only > Spotlight > Don't Show in Menu Bar
-    defaults write com.apple.Spotlight MenuItemHidden -int 1
-
-    # Decrease menu bar items spacing
-    defaults write NSGlobalDomain NSStatusItemSpacing -int 10
-
-    # Show app switcher on all displays (default: Dock display only)
-    defaults write com.apple.dock appswitcher-all-displays -bool true
-
-    # Desktop & Dock -> Windows -> tiled windows have margins: disable
-    defaults write com.apple.WindowManager EnableTiledWindowMargins -bool false
-
-    # Desktop & Dock -> automatically rearrange Spaces based on most recent use: disable
-    defaults write com.apple.dock workspaces-auto-swoosh -bool false
-
-    # Desktop & Dock -> when switching to an application, switch to a space with open windows for the application: enable
-    defaults write com.apple.dock AppleSpacesSwitchOnActivate -bool true
-
-    # Desktop & Dock -> desktop & stage manager -> click wallpaper to reveal desktop: only in stage manager
-    defaults write com.apple.dock showDesktopOnlyInSM -bool true
-
-    # Desktop & Dock -> hot corners -> upper left: mission control
-    defaults write com.apple.dock wvous-tl-corner -int 2
-    defaults write com.apple.dock wvous-tl-modifier -int 0
-
-    # Desktop & Dock -> Windows & Apps > Prefer tabs when opening documents: always
-    defaults write NSGlobalDomain AppleWindowTabbingMode -string "always"
-
-    ## Spotlight
-
-    defaults write com.apple.Spotlight EnabledPreferenceRules -array "Custom.relatedContents" "com.apple.iBooksX" "com.hnc.Discord" \
-        "com.apple.mail" "com.apple.podcasts" "com.apple.Safari" "net.whatsapp.WhatsApp" "com.microsoft.rdc.macos"
-    defaults write com.apple.Spotlight PasteboardHistoryEnabled -int 1
-    defaults write com.apple.Spotlight PasteboardHistoryTimeout -int 1800
-
-    ## Finder
-
-    # Finder > Preferences > Show all filename extensions
-    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-    # Allow quitting Finder via ⌘ + Q; doing so will also hide desktop icons
-    defaults write com.apple.finder QuitMenuItem -bool true
-
-    # Finder > View > Show Path Bar
-    defaults write com.apple.finder ShowPathbar -bool true
-
-    # Finder > General > New Finder window show: Home Directory
-    defaults write com.apple.finder NewWindowTarget -string "PfHm"
-    defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
-
-    # Finder > Advanced > Remove items from the Bin after 30 days: enable
-    defaults write com.apple.finder FXRemoveOldTrashItems -bool true
-
-    # Finder > Advanced > Keep folders on top: in windows when sorting by name
-    defaults write com.apple.finder _FXSortFoldersFirst -bool true
-
-    # Finder > Advanced > When performing a search: search the current folder
-    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-
-    # Finder > default view > List View
-    defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-    # Show hidden ~/Library folder in Finder
-    chflags nohidden ~/Library
-
-    ## Keyboard
-
-    # Setup system shorcuts
-    setup_mac_shortcuts
-
-    # Keyboard -> delay until repeat: 15*15 ms; key repeat rate: 2*15 ms
-    defaults write NSGlobalDomain InitialKeyRepeat -int 15
-    defaults write NSGlobalDomain KeyRepeat -int 2
-
-    # Keyboard -> keyboard navigation: enable
-    defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-    # Keyboard -> configure single press of fn key to "do nothing"
-    defaults write com.apple.HIToolbox AppleFnUsageType -int 0
-
-    ## Mouse
-
-    # Disable mouse acceleration
-    defaults write NSGlobalDomain com.apple.mouse.scaling -1
-
-    ## Battery
-
-    # Battery -> options -> slightly dim the display on battery: disable
-    defaults write com.apple.controlcenter DimDisplayOnBattery -bool false
-
-    ## Misc
-
-    # Avoid creation of .DS_Store files on network volumes
-    defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-    # Set screenshots location to ~/Pictures/ss (default: ~/Desktop)
-    screenshotDir="${HOME}/Pictures/ss"
-    mkdir -p "${screenshotDir}"
-    defaults write com.apple.screencapture location -string "${screenshotDir}"
-
-    ## External apps
-
-    # Use cmd+tab and cmd+~ to switch apps in Alt-Tab
-    defaults write com.lwouis.alt-tab-macos holdShortcut "\\U2318"
-    defaults write com.lwouis.alt-tab-macos holdShortcut2 "\\U2318"
-
-    ## Apply changes
-    echo "Restarting system processes to apply changes..."
-    killall cfprefsd # Restart the preferences daemon to ensure all plist changes are applied
-    killall Dock Finder SystemUIServer
-    echo "Changes applied. For some keyboard settings, you may need to log out and log back in."
-}
-
-setup_mac_shortcuts() {
-    # Keyboard -> shortcuts -> launchpad & dock -> disable: turn dock hiding on/off (ID 52)
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 52 "{enabled = 0; value = { parameters = (100, 2, 1572864); type = 'standard'; };}"
-
-    # Keyboard -> shortcuts -> mission control -> mission control: ctrl+cmd+S
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 32 "{enabled = 1; value = { parameters = (115, 1, 1310720); type = standard; };}"
-
-    # Keyboard -> shortcuts -> mission control -> application windows: ctrl+cmd+A
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 33 "{enabled = 1; value = { parameters = (97, 0, 1310720); type = 'standard'; };}"
-
-    # Keyboard -> shortcuts -> mission control -> move left a space: ctrl+cmd+left
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 79 "{enabled = 1; value = { parameters = (65535, 123, 9699328); type = standard; };}"
-
-    # Keyboard -> shortcuts -> mission control -> move right a space: ctrl+cmd+right
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 81 "{enabled = 1; value = { parameters = (65535, 124, 9699328); type = standard; };}"
-
-    # Keyboard -> shortcuts -> mission control -> disable "Show Desktop"
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 36 "{enabled = 0; value = { parameters = (65535, 103, 8388608); type = 'standard'; };}"
-
-    # Keyboard -> shortcuts -> mission control -> disable "Quick Note"
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 190 "{enabled = 0; value = { parameters = (113, 12, 8388608); type = 'standard'; };}"
-
-    # Keyboard -> shortcuts -> input sources -> disable: select next/previous input source
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 60 "{enabled = 0; value = { parameters = (32, 49, 262144); type = 'standard'; };}"
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 61 "{enabled = 0; value = { parameters = (32, 49, 786432); type = 'standard'; };}"
-
-    # Keyboard -> shortcuts -> accessibility -> disable: show accessibility controls
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 162 "{enabled = 0; value = { parameters = (65535, 96, 9961472); type = 'standard'; };}"
-
-    # Keyboard -> shortcuts -> accessibility -> disable: turn VoiceOver on or off
-    defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 59 "{enabled = 0; value = { parameters = (65535, 96, 9437184); type = 'standard'; };}"
-
-    # Keyboard -> shortcuts -> services -> searching -> disable: search with google
-    defaults write pbs NSServicesStatus -dict-add 'com.apple.Safari - Search With %WebSearchProvider@ - searchWithWebSearchProvider' '{"enabled_context_menu" = 0; "enabled_services_menu" = 0; "presentation_modes" = { ContextMenu = 0; ServicesMenu = 0; }; }'
-
-    # Keyboard -> shortcuts -> services -> text -> disable: various text services
-    defaults write pbs NSServicesStatus -dict-add 'com.apple.ChineseTextConverterService - Convert Text from Traditional to Simplified Chinese - convertTextToSimplifiedChinese' '{"enabled_context_menu" = 0; "enabled_services_menu" = 0; "presentation_modes" = { ContextMenu = 0; ServicesMenu = 0; }; }'
-    defaults write pbs NSServicesStatus -dict-add 'com.apple.ChineseTextConverterService - Convert Text from Simplified to Traditional Chinese - convertTextToTraditionalChinese' '{"enabled_context_menu" = 0; "enabled_services_menu" = 0; "presentation_modes" = { ContextMenu = 0; ServicesMenu = 0; }; }'
-    defaults write pbs NSServicesStatus -dict-add 'com.apple.Stickies - Make Sticky - makeStickyFromTextService' '{"enabled_services_menu" = 0; "presentation_modes" = { ContextMenu = 0; ServicesMenu = 0; }; }'
-    defaults write pbs NSServicesStatus -dict-add 'com.apple.Terminal - Open man Page in Terminal - openManPage' '{"enabled_context_menu" = 0; "enabled_services_menu" = 0; "presentation_modes" = { ContextMenu = 0; ServicesMenu = 0; }; }'
-    defaults write pbs NSServicesStatus -dict-add 'com.apple.Terminal - Search man Page Index in Terminal - searchManPages' '{"enabled_context_menu" = 0; "enabled_services_menu" = 0; "presentation_modes" = { ContextMenu = 0; ServicesMenu = 0; }; }'
 }
 
 install_ohmyzsh() {
